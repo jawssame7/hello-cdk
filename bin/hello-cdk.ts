3b25f4751e8ac9526c7f32f9b87ca6773c00c515
鮫島泰史 <h.sameshima@wiz-net.jp>
2024-12-22 14:46:49 +0900

    first commit

--- bin/hello-cdk.ts
+++ bin/hello-cdk.ts
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
-import * as cdk from 'aws-cdk-lib';
-import { HelloCdkStack } from '../lib/hello-cdk-stack';
+import * as cdk from "aws-cdk-lib";
+import { HelloCdkStack } from "../lib/hello-cdk-stack";
 
 const app = new cdk.App();
-new HelloCdkStack(app, 'HelloCdkStack', {
+new HelloCdkStack(app, "HelloCdkStack", {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
@@ -17,4 +17,5 @@ new HelloCdkStack(app, 'HelloCdkStack', {
   // env: { account: '123456789012', region: 'us-east-1' },
 
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+  env: { account: "703671935472", region: "ap-northeast-1" },
 });
--- lib/hello-cdk-stack.ts
+++ lib/hello-cdk-stack.ts
@@ -1,6 +1,9 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import * as cdk from "aws-cdk-lib";
+import { Lambda } from "aws-cdk-lib/aws-ses-actions";
+import { Construct } from "constructs";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
+// Import the Lambda module
+import * as lambda from "aws-cdk-lib/aws-lambda";
 
 export class HelloCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -12,5 +15,30 @@ export class HelloCdkStack extends cdk.Stack {
     // const queue = new sqs.Queue(this, 'HelloCdkQueue', {
     //   visibilityTimeout: cdk.Duration.seconds(300)
     // });
+
+    // defien lambda function
+
+    const myFunction = new lambda.Function(this, "HelloWorldFunction", {
+      runtime: lambda.Runtime.NODEJS_20_X,
+      handler: "index.handler",
+      code: lambda.Code.fromInline(`
+        exports.handler = async (evnet) => {
+          return {
+            statusCode: 200,
+            body: JSON.stringify('Hello CDK!! from Lambda!'),
+          };
+        };
+      `),
+    });
+
+    // define lambda function URL resource
+    const myFunctionUrl = myFunction.addFunctionUrl({
+      authType: lambda.FunctionUrlAuthType.NONE,
+    });
+
+    // define a cloudformation output for url
+    new cdk.CfnOutput(this, "myFunctionUrlOutput", {
+      value: myFunctionUrl.url,
+    });
   }
 }
